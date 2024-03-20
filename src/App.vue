99a1b2176cc70683f8c1a4b084f327392dc8a6d9
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-03-20 20:27:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="max-w-5xl mx-auto my-6">
+      <h1 class="text-3xl font-bold text-gray-900">
+        Hello, Vue 3!
+      </h1>
+      <p class="mt-4 text-gray-600">
+        This is a Vue 3 app with Vite and Tailwind CSS.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {},
+})
+</script>
