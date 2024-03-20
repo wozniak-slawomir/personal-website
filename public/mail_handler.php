e4be68d849ebb8fbd3b666a116024a4e0e3b9c16
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-03-20 20:27:09 +0100

    Add mail_handler to public folder

---
+++ public/mail_handler.php
@@ -0,0 +1,22 @@
+<?php
+    ini_set("SMTP", "smtp.slawomir-wozniak.pl");
+    ini_set("sendmail_from", "postmaster@slawomir-wozniak.pl");
+
+    $headers ="From: $_POST[email]"."\n";
+    $headers .='Reply-To: contact@slawomir-wozniak.pl'."\n";
+    $headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n";
+    $headers .='Content-Transfer-Encoding: 8bit';
+
+    if (
+        mail(
+            'contact@slawomir-wozniak.pl', 
+            "Message from the personal website", 
+            "Contact number: ".$_POST['phone-number']." Message: ".$_POST['message'],
+            $headers
+        ) 
+    ){
+        header('Location: /?mail=success');
+    } else {
+        header('Location: /?mail=error');
+    }
+?>
