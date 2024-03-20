3ae89604446288ea866a69a792998162492a925a
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-03-20 20:27:09 +0100

    Fix logo name resolution

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -2,7 +2,7 @@
   <nav
     class="w-full h-[var(--navbar-height)] bg-[color:var(--secondary-color)] py-5 px-16 flex flex-wrap justify-between items-center fixed top-0 left-0"
   >
-    <img src="logo.png" class="w-[400px]" />
+    <img src="/logo.png" class="w-[400px]" />
     <div class="h-full">
       <button
         class="px-8 py-5 rounded-full shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] bg-[color:var(--secondary-color)] hidden md:inline-block font-semibold bg-[image:var(--primary-gradient)] text-black rounded-full transition-transform transform-gpu hover:-translate-y-1"
