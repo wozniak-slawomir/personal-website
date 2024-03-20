b631c3e597586307534fced2fda6868ded745ff8
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-03-20 20:27:08 +0100

    Add swiper with partners

--- src/Hero.vue
+++ src/Hero.vue
@@ -5,21 +5,50 @@
     >
       Create<span
         class="text-[color:var(--secondary-color)] text-7xl leading-[0]"
-      >.</span><br>
+        >.</span
+      ><br />
       Develop<span
         class="text-[color:var(--secondary-color)] text-7xl leading-[0]"
-      >.</span><br>
+        >.</span
+      ><br />
       Achieve<span
         class="text-[color:var(--secondary-color)] text-7xl leading-[0]"
-      >.</span>
+        >.</span
+      >
     </h1>
+
+    <div class="partners">
+      <swiper-container
+        slides-per-view="4"
+        class="mix-blend-luminosity opacity-85"
+      >
+        <swiper-slide v-for="partner in partners" :key="partner.alt">
+          <img class="h-20" :src="partner.src" :alt="partner.alt" />
+        </swiper-slide>
+      </swiper-container>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Hero',
-}
+<script setup>
+const partners = [
+  {
+    alt: "Webwave",
+    src: "/partners/webwave.svg",
+  },
+  {
+    alt: "Nordea",
+    src: "/partners/Nordea.png",
+  },
+  {
+    alt: "Wirtualna Polska",
+    src: "/partners/wp.png",
+  },
+  {
+    alt: "Cognitran",
+    src: "/partners/cognitran.svg",
+  },
+];
 </script>
 
 <style lang="css" scoped>
@@ -34,5 +63,35 @@ export default {
 
 .keywords {
   grid-area: keywords;
+}
+
+.partners {
+  grid-area: partners;
+}
+
+swiper-container {
+  height: 100%;
+  max-height: 100vw;
+  min-height: 0;
+  min-width: 0;
+  max-width: 100vw;
+  width: 100%;
+  overflow: hidden;
+}
+
+swiper-slide {
+  width: auto;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  max-height: 100%;
+}
+
+swiper-wrapper {
+  max-height: 100%;
+  height: 100%t;
+  display: flex;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { register } from 'swiper/element/bundle'
+import 'swiper/css/bundle';
 import App from './App.vue'
 import './index.css'
+
+register()
 
 const app = createApp(App)
 
