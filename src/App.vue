5990b94211c28813076b216dc93a793e8adc96a0
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-03-20 20:27:08 +0100

    Add basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-    <div class="max-w-5xl mx-auto my-6">
-      <h1 class="text-3xl font-bold text-gray-900">
-        Hello, Vue 3!
-      </h1>
-      <p class="mt-4 text-gray-600">
-        This is a Vue 3 app with Vite and Tailwind CSS.
-      </p>
-    </div>
+  <div class="text-[color:var(--primary-text-color)]">
+    <Navbar />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Navbar from './Navbar.vue'
 
 export default defineComponent({
-  components: {},
+  components: {Navbar},
 })
 </script>
---
+++ src/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav
+    class="w-full h-[88px] bg-[color:var(--primary-color)] py-5 px-16 flex justify-between items-center fixed top-0 left-0"
+  >
+    <img src="logo.png" class="h-full" />
+    <div class="flex gap-8">
+      <a
+        href="#"
+        class="text-[color:var(--secondary-text-color)] uppercase hover:text-[color:var(--primary-text-color)]"
+        >Home</a
+      >
+      <a
+        href="#"
+        class="text-[color:var(--secondary-text-color)] uppercase hover:text-[color:var(--primary-text-color)]"
+        >About</a
+      >
+      <a
+        href="#"
+        class="text-[color:var(--secondary-text-color)] uppercase hover:text-[color:var(--primary-text-color)]"
+        >Contact</a
+      >
+    </div>
+    <div class="h-full">
+      <button
+        class="px-10 h-full rounded-full bg-[color:var(--secondary-color)]"
+      >
+        Contact me
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  // Your component's logic goes here
+};
+</script>
+
+<style scoped>
+/* Your component's styles go here */
+</style>
