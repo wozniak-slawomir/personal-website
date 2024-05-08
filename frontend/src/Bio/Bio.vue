36b49a2bc094640c322206d89b7cd0c6e06f288b
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-05-08 10:58:32 +0200

    Create a container class

--- frontend/src/Bio/Bio.vue
+++ frontend/src/Bio/Bio.vue
@@ -1,102 +1,58 @@
 <template>
-  <div class="mx-64 flex flex-col">
+  <div class="container">
     <h1 class="text-5xl font-bold">BIO</h1>
     <div class="mt-10 flex">
-      <button
-        @click="bioState = 'story'"
-        v-bind:class="bioState === 'story' ? 'bg-[#474b59]' : 'bg-[#393939]'"
-        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow"
-      >
+      <button @click="bioState = 'story'" v-bind:class="bioState === 'story' ? 'bg-[#474b59]' : 'bg-[#393939]'"
+        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow">
         STORY
       </button>
-      <button
-        @click="bioState = 'career'"
-        v-bind:class="bioState === 'career' ? 'bg-[#474b59]' : 'bg-[#393939]'"
-        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow"
-      >
+      <button @click="bioState = 'career'" v-bind:class="bioState === 'career' ? 'bg-[#474b59]' : 'bg-[#393939]'"
+        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow">
         CAREER
       </button>
-      <button
-        @click="bioState = 'achievements'"
-        v-bind:class="
-          bioState === 'achievements' ? 'bg-[#474b59]' : 'bg-[#393939]'
-        "
-        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow"
-      >
+      <button @click="bioState = 'achievements'" v-bind:class="bioState === 'achievements' ? 'bg-[#474b59]' : 'bg-[#393939]'
+        " class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow">
         ACHIEVEMENTS
       </button>
-      <button
-        @click="bioState = 'personal life'"
-        v-bind:class="
-          bioState === 'personal life' ? 'bg-[#474b59]' : 'bg-[#393939]'
-        "
-        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow"
-      >
+      <button @click="bioState = 'personal life'" v-bind:class="bioState === 'personal life' ? 'bg-[#474b59]' : 'bg-[#393939]'
+        " class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow">
         PERSONAL LIFE
       </button>
-      <button
-        @click="bioState = 'skills'"
-        v-bind:class="bioState === 'skills' ? 'bg-[#474b59]' : 'bg-[#393939]'"
-        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow"
-      >
+      <button @click="bioState = 'skills'" v-bind:class="bioState === 'skills' ? 'bg-[#474b59]' : 'bg-[#393939]'"
+        class="text-l p-3 rounded-3xl hover:bg-[#464646] h-15 mr-5 px-5 duration-300 shadow">
         SKILLS
       </button>
     </div>
     <div>
       <div class="flex">
-        <div
-          v-bind:class="
-            bioState === 'story' ? ' translate-y-[-0px]' : 'translate-y-[25px]'
-          "
-          class="translate-x-1 transition-all mb-[-35px] mt-[-10px]"
-        >
+        <div v-bind:class="bioState === 'story' ? ' translate-y-[-0px]' : 'translate-y-[25px]'
+        " class="translate-x-1 transition-all mb-[-35px] mt-[-10px]">
           <PhCaretUp weight="fill" :size="80" :color="'#393939'" />
         </div>
-        <div
-          v-bind:class="
-            bioState === 'career' ? ' translate-y-[-0px]' : 'translate-y-[25px]'
-          "
-          class="translate-x-[37px] transition-all mb-[-35px] mt-[-10px]"
-        >
+        <div v-bind:class="bioState === 'career' ? ' translate-y-[-0px]' : 'translate-y-[25px]'
+        " class="translate-x-[37px] transition-all mb-[-35px] mt-[-10px]">
           <PhCaretUp weight="fill" :size="80" :color="'#393939'" />
         </div>
-        <div
-          v-bind:class="
-            bioState === 'achievements'
-              ? ' translate-y-[-0px]'
-              : 'translate-y-[25px]'
-          "
-          class="translate-x-[105px] transition-all mb-[-35px] mt-[-10px]"
-        >
+        <div v-bind:class="bioState === 'achievements'
+          ? ' translate-y-[-0px]'
+          : 'translate-y-[25px]'
+        " class="translate-x-[105px] transition-all mb-[-35px] mt-[-10px]">
           <PhCaretUp weight="fill" :size="80" :color="'#393939'" />
         </div>
-        <div
-          v-bind:class="
-            bioState === 'personal life'
-              ? ' translate-y-[-0px]'
-              : 'translate-y-[25px]'
-          "
-          class="translate-x-[207px] transition-all mb-[-35px] mt-[-10px]"
-        >
+        <div v-bind:class="bioState === 'personal life'
+          ? ' translate-y-[-0px]'
+          : 'translate-y-[25px]'
+        " class="translate-x-[207px] transition-all mb-[-35px] mt-[-10px]">
           <PhCaretUp weight="fill" :size="80" :color="'#393939'" />
         </div>
-        <div
-          v-bind:class="
-            bioState === 'skills' ? ' translate-y-[-0px]' : 'translate-y-[25px]'
-          "
-          class="translate-x-[270px] transition-all mb-[-35px] mt-[-10px]"
-        >
+        <div v-bind:class="bioState === 'skills' ? ' translate-y-[-0px]' : 'translate-y-[25px]'
+        " class="translate-x-[270px] transition-all mb-[-35px] mt-[-10px]">
           <PhCaretUp weight="fill" :size="80" :color="'#393939'" />
         </div>
       </div>
-      <div
-        class="bg-[#393939] rounded-2xl w-12/12 flex justify-between flex-col p-8 mt-0 shadow"
-      >
-        <p
-          v-bind:class="expandBio ? ' max-h-full' : 'max-h-[90px]'"
-          v-if="bioState === 'story'"
-          class="text-xl mb-10 overflow-clip"
-        >
+      <div class="bg-[#393939] rounded-2xl w-12/12 flex justify-between flex-col p-8 mt-0 shadow">
+        <p v-bind:class="expandBio ? ' max-h-full' : 'max-h-[90px]'" v-if="bioState === 'story'"
+          class="text-xl mb-10 overflow-clip">
           STORY Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           Doloribus quia totam consectetur cum placeat eligendi iusto eos libero
           rem repellat obcaecati provident nam quam veniam magnam, aliquid
@@ -124,19 +80,13 @@
           PERSONAL LIFE
         </p>
         <p v-if="bioState === 'skills'" class="text-xl mb-10">SKILLS</p>
-        <button
-          v-if="expandBio"
-          @click="expandBio = !expandBio"
-          class="self-start text-sm"
-        >
-          SEE LESS<PhCaretUp :size="20" class="inline align-bottom ml-2" />
+        <button v-if="expandBio" @click="expandBio = !expandBio" class="self-start text-sm">
+          SEE LESS
+          <PhCaretUp :size="20" class="inline align-bottom ml-2" />
         </button>
-        <button
-          v-if="!expandBio"
-          @click="expandBio = !expandBio"
-          class="self-start text-sm"
-        >
-          SEE MORE<PhCaretDown :size="20" class="inline align-bottom ml-2" />
+        <button v-if="!expandBio" @click="expandBio = !expandBio" class="self-start text-sm">
+          SEE MORE
+          <PhCaretDown :size="20" class="inline align-bottom ml-2" />
         </button>
       </div>
     </div>
