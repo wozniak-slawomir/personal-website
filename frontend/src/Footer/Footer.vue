ef53d26e61c02a3af2869d0902c90ea20b50a877
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-05-14 11:39:38 +0200

    fix container width; add important to tailwind

--- frontend/src/Footer/Footer.vue
+++ frontend/src/Footer/Footer.vue
@@ -1,22 +1,19 @@
 <template>
-  <footer class="bg-[--secondary-color] px-10 py-10 mt-32 text-gray-300 lg:px-64">
+  <footer class="bg-[--secondary-color] container py-10 !mt-32 text-gray-300">
     <div class="flex justify-between items-center flex-col lg:flex-row gap-5">
       <img src="/logo.png" alt="logo" class="max-w-[400px] w-full lg:h-[30px]" />
       <div class="flex gap-5 text-white">
         <a href="https://www.linkedin.com/in/slawek-wozniak/" target="_blank">
-          <PhLinkedinLogo :size="45" class="h-12"/>
+          <PhLinkedinLogo :size="45" class="h-12" />
         </a>
         <a href="https://github.com/wozniak-slawomir" target="_blank">
-          <PhGithubLogo :size="45" class="h-12"/>
+          <PhGithubLogo :size="45" class="h-12" />
         </a>
         <a href="mailto:wozniakslawomir.contact@gmail.com" target="_blank">
-          <PhEnvelopeSimple :size="45" class="h-12"/>
+          <PhEnvelopeSimple :size="45" class="h-12" />
         </a>
-        <a
-          href="https://www.instagram.com/wozniaakslawek"
-          target="_blank"
-        >
-          <PhInstagramLogo :size="45" class="h-12"/>
+        <a href="https://www.instagram.com/wozniaakslawek" target="_blank">
+          <PhInstagramLogo :size="45" class="h-12" />
         </a>
       </div>
     </div>
