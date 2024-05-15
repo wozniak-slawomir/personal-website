d07927d2d6cf90e60513b5eb65bc64d646a825d6
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-05-15 21:38:48 +0200

    Delete unused code

--- frontend/src/Testimonials/Testimonials.vue
+++ frontend/src/Testimonials/Testimonials.vue
@@ -2,7 +2,7 @@
     <div class="container mt-10 w-full overflow-hidden">
         <div class="h-[450px] container absolute scale-[101%] translate-y-36 translate-x-[-20px] bg-gradient-to-r from-[var(--secondary-color)] via-transparent to-[var(--secondary-color)] z-10 md:translate-y-32"></div>
         <h1 class="text-5xl font-bold my-10 text-center md:text-left">WHAT OUR CLIENTS SAY</h1>
-        <div class="flex bg-gradient-to-r from-[var(--secondary-color)] via-transparent to-[var(--secondary-color)] animate-slide whitespace-nowrap">
+        <div class="flex animate-slide whitespace-nowrap">
             <div v-for="topTestimonials in topTestimonials" class="w-full inline-block whitespace-normal">
                     <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6 mb-6">
                     <h1 class="text-3xl">{{ topTestimonials.text }}</h1>
@@ -22,8 +22,8 @@
                 </div>
             </div>
         </div>
-        <div class="flex ml-[-175px] animate-slide">
-            <div v-for="bottomTestimonials in bottomTestimonials" class="w-full">
+        <div class="flex ml-[-175px] animate-slide whitespace-nowrap">
+            <div v-for="bottomTestimonials in bottomTestimonials" class="w-full inline-block whitespace-normal">
                     <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6">
                     <h1 class="text-3xl">{{ bottomTestimonials.text }}</h1>
                     <div class="flex flex-col text-[#929292]">
@@ -32,7 +32,7 @@
                     </div>
                 </div>
             </div>
-            <div v-for="bottomTestimonials in bottomTestimonials" class="w-full">
+            <div v-for="bottomTestimonials in bottomTestimonials" class="w-full inline-block whitespace-normal">
                     <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6">
                     <h1 class="text-3xl">{{ bottomTestimonials.text }}</h1>
                     <div class="flex flex-col text-[#929292]">
@@ -113,7 +113,3 @@
         },
     ];
 </script>
-
-<style scoped lang="css">
-
-</style>
