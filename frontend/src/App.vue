e239ff738151db036ef461e35cff59c357e5ef35
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-05-18 20:32:41 +0200

    Add contact modal functionality to Navbar and Hero components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <Navbar />
-  <Hero />
+  <Navbar @showContactModal="showContactModal" />
+  <Hero @showContactModal="showContactModal" />
   <Bio />
+  <ContactModal @hideContactModal="hideContactModal" v-if="renderContactModal"/>
   <Footer />
 </template>
 
@@ -11,8 +12,23 @@ import Navbar from "./Navbar.vue";
 import Hero from "./Hero.vue";
 import Bio from "./Bio/Bio.vue";
 import Footer from "./Footer/Footer.vue";
+import ContactModal from "./ContactModal/ContactModal.vue";
 
 export default defineComponent({
-  components: { Navbar, Hero, Bio, Footer },
-});
+  components: { Navbar, Hero, Bio, Footer, ContactModal },
+
+  data() {
+    return {
+      renderContactModal: false,
+    };
+  },
+  methods: {
+    showContactModal() {
+      this.renderContactModal = true;
+    },
+    hideContactModal() {
+      this.renderContactModal = false;
+    },
+  },
+  });
 </script>
