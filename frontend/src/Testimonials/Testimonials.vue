1df683a27f1669ef695399ebd069764b2199e4bc
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-05-25 15:30:48 +0200

    Fix after CR and add testimonials content

--- frontend/src/Testimonials/Testimonials.vue
+++ frontend/src/Testimonials/Testimonials.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="container mt-10 w-full overflow-hidden">
-        <div class="h-[450px] container absolute scale-[101%] translate-y-36 translate-x-[-20px] bg-gradient-to-r from-[var(--secondary-color)] via-transparent to-[var(--secondary-color)] z-10 md:translate-y-32"></div>
-        <h1 class="text-5xl font-bold my-10 text-center md:text-left">WHAT OUR CLIENTS SAY</h1>
+    <div class="container mt-10 overflow-hidden">
+        <div class="h-[550px] w-full absolute z-10 bg-gradient-to-r from-[var(--secondary-color)] via-transparent to-[var(--secondary-color)] translate-x-[-20px] mt-32 sm:max-w-[640px] md:max-w-[1025px] xl:max-w-[1536px] scale-[101%]"></div>
+        <h1 class="text-5xl font-bold my-10 text-center md:text-left z-20">WHAT OUR CLIENTS SAY</h1>
         <div class="flex animate-slide whitespace-nowrap">
             <div v-for="topTestimonials in topTestimonials" class="w-full inline-block whitespace-normal">
-                    <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6 mb-6">
-                    <h1 class="text-3xl">{{ topTestimonials.text }}</h1>
+                    <div class="flex flex-col w-[350px] h-[250px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6 mb-6">
+                    <h1 class="text-2xl">{{ topTestimonials.text }}</h1>
                     <div class="flex flex-col text-[#929292]">
                         <h3>{{ topTestimonials.name }}</h3>
-                        <p>{{ topTestimonials.position }}, {{ topTestimonials.company }}</p>
+                        <p>{{ topTestimonials.position }}{{ topTestimonials.company }}</p>
                     </div>
                 </div>
             </div>
             <div v-for="topTestimonials in topTestimonials" class="w-full inline-block whitespace-normal">
-                    <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6 mb-6">
-                    <h1 class="text-3xl">{{ topTestimonials.text }}</h1>
+                    <div class="flex flex-col w-[350px] h-[250px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6 mb-6">
+                    <h1 class="text-2xl">{{ topTestimonials.text }}</h1>
                     <div class="flex flex-col text-[#929292]">
                         <h3>{{ topTestimonials.name }}</h3>
-                        <p>{{ topTestimonials.position }}, {{ topTestimonials.company }}</p>
+                        <p>{{ topTestimonials.position }}{{ topTestimonials.company }}</p>
                     </div>
                 </div>
             </div>
         </div>
         <div class="flex ml-[-175px] animate-slide whitespace-nowrap">
             <div v-for="bottomTestimonials in bottomTestimonials" class="w-full inline-block whitespace-normal">
-                    <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6">
-                    <h1 class="text-3xl">{{ bottomTestimonials.text }}</h1>
+                    <div class="flex flex-col w-[350px] h-[250px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6">
+                    <h1 class="text-2xl">{{ bottomTestimonials.text }}</h1>
                     <div class="flex flex-col text-[#929292]">
                         <h3>{{ bottomTestimonials.name }}</h3>
-                        <p>{{ bottomTestimonials.position }}, {{ bottomTestimonials.company }}</p>
+                        <p>{{ bottomTestimonials.position }}{{ bottomTestimonials.company }}</p>
                     </div>
                 </div>
             </div>
             <div v-for="bottomTestimonials in bottomTestimonials" class="w-full inline-block whitespace-normal">
-                    <div class="flex flex-col w-[350px] h-[200px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6">
-                    <h1 class="text-3xl">{{ bottomTestimonials.text }}</h1>
+                    <div class="flex flex-col w-[350px] h-[250px] p-7 justify-between rounded-2xl bg-gradient-to-t from-[var(--secondary-color)] to-[#393939] mr-6">
+                    <h1 class="text-2xl">{{ bottomTestimonials.text }}</h1>
                     <div class="flex flex-col text-[#929292]">
                         <h3>{{ bottomTestimonials.name }}</h3>
-                        <p>{{ bottomTestimonials.position }}, {{ bottomTestimonials.company }}</p>
+                        <p>{{ bottomTestimonials.position }}{{ bottomTestimonials.company }}</p>
                     </div>
                 </div>
             </div>
@@ -45,71 +45,47 @@
     </div>
 </template>
 <script setup lang="ts">
-// najlepiej jakby było tyle samo górnych i dolnych testimoniali bo animacja ładnie chodzi, jak będzie więcej niż 5 to trzeba zmienić w configu animację
+// the number of top and bottom testimonials should be the same, otherwise the animation will not work, in case more is needed, the animation should be adjusted
     const topTestimonials = [
         {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "1",
+            text: "He was my front-end technical lead and I relied on him greatly",
+            name: "Paul Richardson",
+            position: "Senior Project Manager",
+            company: " of Cognitran Ltd",
         },
         {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "2",
+            text: "Their proficiency in React is outstanding, Sławomir is not only skilled but also proactive",
+            name: "Daniel Sobisz",
+            position: "Front-End Developer",
+            company: "",
         },
         {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "3",
-        },
-        {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "4",
-        },
-        {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "5",
+            text: "He is an excellent communicator and a great team player, a reliable and valuable asset to any team.",
+            name: "Adam Wuss",
+            position: "Senior Frontend Developer",
+            company: "",
         },
         
     ];
 
     const bottomTestimonials = [
         {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "1",
+            text: "Sławek is a professional with an open mind. He is full of ideas not only for new projects but also for solving potential problems.",
+            name: "Anna Wójcik",
+            position: "Healthcare | IT | Public relations | Media",
+            company: "",
         },
         {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "2",
+            text: "Their mastery of React, Vue, and JavaScript is truly impressive,",
+            name: "Piotr Świerszcz",
+            position: "Senior Frontend Developer",
+            company: "",
         },
         {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
+            text: "I am very pleased with Sławek's work, full professionalism and effective cooperation.",
+            name: "Kamil Hiszpan",
             position: "CEO",
-            company: "3",
-        },
-        {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "4",
-        },
-        {
-            text: "Never a downtime. Place of mind.",
-            name: "John Doe",
-            position: "CEO",
-            company: "5",
+            company: " of Hiszpanbet",
         },
     ];
 </script>
