1d857d2f47dbe317c7df1b68c7e1722875df6de1
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-06-05 15:03:19 +0200

    Remove disabled state from the button

--- frontend/src/ContactModal/ContactModal.vue
+++ frontend/src/ContactModal/ContactModal.vue
@@ -50,7 +50,6 @@
                             class="w-full p-2 rounded-lg bg-[#252525] border-0 min-h-[150px] resize-none"></textarea>
                     </div>
                     <button
-                        :disabled="isLoading"
                         class="uppercase block py-2 px-6 mx-auto my-0 rounded-lg bg-[color:var(--secondary-color)] font-semibold bg-[image:var(--primary-gradient)] text-black transition-all transform-gpu hover:-translate-y-1 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] hover:bg-[image:var(--secondary-gradient)] active:opacity-50 duration-300">Send</button>
                 </form>
             </div>
