c5b6912c127f2cd52ab441e12d2f214e14b53e6c
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-08-09 14:18:29 +0200

    Complete section design and functionality

--- frontend/components/RealizedProjects.vue
+++ frontend/components/RealizedProjects.vue
@@ -6,58 +6,65 @@
       </h1>
       <div class="mb-5 mt-10 flex flex-wrap lg:my-10 gap-5">
         <button
-          :class="{ 'bg-[#474b59]': activeFilter === 'CATEGORY 1' }"
-          class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow lg:max-w-fit lg:px-20 lg:mb-0 "
-          @click="activeFilter === 'CATEGORY 1' ? activeFilter = 'ALL' : activeFilter = 'CATEGORY 1'"
+          :class="{ 'bg-[#474b59]': activeFilter === 'business' }"
+          class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow uppercase lg:max-w-fit lg:px-20 lg:mb-0"
+          @click="activeFilter === 'business' ? activeFilter = 'all' : activeFilter = 'business'"
         >
-          Category 1
+          business
         </button>
         <button
-          :class="{ 'bg-[#474b59]': activeFilter === 'CATEGORY 2' }"
-          class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow lg:max-w-fit lg:px-20 lg:mb-0"
-          @click="activeFilter === 'CATEGORY 2' ? activeFilter = 'ALL' : activeFilter = 'CATEGORY 2'"
+          :class="{ 'bg-[#474b59]': activeFilter === 'portfolio' }"
+          class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow uppercase lg:max-w-fit lg:px-20 lg:mb-0"
+          @click="activeFilter === 'portfolio' ? activeFilter = 'all' : activeFilter = 'portfolio'"
         >
-          Category 2
+          portfolio
         </button>
         <button
-          :class="{ 'bg-[#474b59]': activeFilter === 'CATEGORY 3' }"
-          class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow lg:max-w-fit lg:px-20 lg:mb-0"
-          @click="activeFilter === 'CATEGORY 3' ? activeFilter = 'ALL' : activeFilter = 'CATEGORY 3'"
+          :class="{ 'bg-[#474b59]': activeFilter === 'blog' }"
+          class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow uppercase lg:max-w-fit lg:px-20 lg:mb-0"
+          @click="activeFilter === 'blog' ? activeFilter = 'all' : activeFilter = 'blog'"
         >
-          Category 3
+          blog
         </button>
       </div>
       <masonry-wall
         :items="filteredItems"
         :gap="24"
         :max-columns="3"
-        :min-columns="3"
-        :column-width="300"
       >
         <template #default="{item}">
-          <div class="relative rounded-2xl overflow-hidden">
+          <div class="relative rounded-2xl overflow-hidden group min-h-fit">
             <img
               :src="item.image"
               :alt="item.name"
-              class="w-full h-fit object-cover"
+              class="w-full h-full object-cover"
             >
-            <!-- <div class="absolute bottom-0 left-0 right-0 bg-[#393939] bg-opacity-90 p-5">
-              <h1 class="text-2xl font-bold text-white">
+            <div class="bottom-0 left-0 right-0 backdrop-blur-md bg-[#393939] p-5 opacity-100 group-hover:opacity-100 transition-opacity duration-300 xl:backdrop-brightness-50 xl:opacity-0 xl:absolute xl:bg-transparent">
+              <h1 class="text-2xl font-bold text-white uppercase">
                 {{ item.name }}
               </h1>
-              <p class="text-white">
+              <p class="text-white my-2">
                 {{ item.description }}
               </p>
-              <div class="flex items-center mt-5">
+              <div class="items-center mt-4 flex justify-between w-full">
                 <a
                   href="#"
-                  class="text-white hover:text-[#f5f5f5] duration-300"
+                  class="bg-white rounded-full text-black px-5 py-1 hover:text-[#f5f5f5] hover:bg-black duration-300"
                 >
                   <span>View project</span>
-                  <PhArrowDownRight class="w-5 h-5 ml-2" />
                 </a>
+                <div class="gap-5 items-center hidden lg:flex">
+                  <p
+                    v-for="tag in item.tags"
+                    :key="tag"
+                    class="underline underline-offset-4 duration-150 capitalize hover:underline-offset-8"
+                  >
+                    {{ tag }}
+                  </p>
+                </div>
+                <PhArrowDownRight class="w-5 h-5" />
               </div>
-            </div> -->
+            </div>
           </div>
         </template>
       </masonry-wall>
@@ -74,36 +81,54 @@ const items = [
     name: 'placeholder 1',
     image: new URL('../assets/projects/placeholder1.png', import.meta.url).href,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
-    tags: ['CATEGORY 1', 'CATEGORY 2'],
+    tags: ['business', 'portfolio'],
   },
   {
     name: 'hiszpanbet',
     image: new URL('../assets/projects/hiszpanbet.png', import.meta.url).href,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
-    tags: ['CATEGORY 1', 'CATEGORY 2', 'CATEGORY 3'],
+    tags: ['business', 'portfolio', 'blog'],
   },
   {
     name: 'placeholder 2',
     image: new URL('../assets/projects/placeholder2.png', import.meta.url).href,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
-    tags: ['CATEGORY 3'],
+    tags: ['blog'],
   },
   {
     name: 'placeholder 3',
-    image: new URL('../assets/projects/placeholder2.png', import.meta.url).href,
+    image: new URL('../assets/projects/placeholder3.png', import.meta.url).href,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
-    tags: ['CATEGORY 3'],
+    tags: ['blog', 'portfolio'],
+  },
+  {
+    name: 'placeholder 4',
+    image: new URL('../assets/projects/placeholder4.jpg', import.meta.url).href,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
+    tags: ['blog', 'portfolio', 'business'],
+  },
+  {
+    name: 'placeholder 5',
+    image: new URL('../assets/projects/placeholder5.png', import.meta.url).href,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
+    tags: ['blog'],
+  },
+  {
+    name: 'placeholder 6',
+    image: new URL('../assets/projects/placeholder6.png', import.meta.url).href,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies. Nullam nec purus nec libero ultricies ultricies.',
+    tags: ['business'],
   },
 ]
 
 //FIX AFTER RELOAD THERE ARE 2 COLUMNS INSTEAD OF 3 DESPITE min-columns=3
 
-type Card = 'CATEGORY 1' | 'CATEGORY 2' | 'CATEGORY 3' | 'ALL'
+type Card = 'business' | 'portfolio' | 'blog' | 'all'
 
-const activeFilter = ref<Card>('ALL')
+const activeFilter = ref<Card>('all')
 
 const filteredItems = computed(() => {
-  if (activeFilter.value === 'ALL') {
+  if (activeFilter.value === 'all') {
     return items
   } else {
     return items.filter((item) => item.tags.includes(activeFilter.value)) 
