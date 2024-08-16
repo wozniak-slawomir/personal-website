286c5e32d402940ef8735a90f2a15700b41096b6
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-08-16 02:46:42 +0200

    Delete unused code

--- frontend/components/BackgroundAnimation.vue
+++ frontend/components/BackgroundAnimation.vue
@@ -50,42 +50,4 @@ onMounted(() => {
 onBeforeUnmount(() => {
   window.removeEventListener('scroll', handleScroll)
 })
-
-// Leftovers from first draft, maybe useful later
-
-// import { ref, onMounted, onBeforeUnmount } from 'vue'
-
-// const count = ref(100)
-
-// const handleScroll = () => {
-//   count.value++
-// }
-
-// onMounted(() => {
-//   window.addEventListener('scroll', handleScroll)
-
-//   const interval = setInterval(() => {
-//     if (count.value > 0) {
-//       count.value = count.value - 2
-//       console.log(count.value)
-//     }
-//   }, 10)
-
-//   onBeforeUnmount(() => {
-//     window.removeEventListener('scroll', handleScroll)
-//     clearInterval(interval)
-//   })
-// })
 </script>
-
-<style scoped>
-@keyframes backgroundAnimation {
-  0% {
-    transform: skewX(0deg);
-  }
-  100% {
-    transform: skewX(12deg);
-  }
-  
-}
-</style>
