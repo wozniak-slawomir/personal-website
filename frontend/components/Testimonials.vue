074bf9a2bd64fd28d48f91f2ededb81f4cf3f9be
Mikołaj Kondela <mkondela.formal@gmail.com>
2024-08-23 15:45:13 +0200

    Fix testimonials

--- frontend/components/Testimonials.vue
+++ frontend/components/Testimonials.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-32 overflow-visible relative">
+  <div class="container w-screen mt-32 overflow-hidden relative xl:overflow-visible">
     <!-- <div
       class="h-full w-full absolute z-10 bg-gradient-to-r from-[var(--secondary-color)] via-transparent to-[var(--secondary-color)] translate-x-[-20px] mt-[136px] sm:mt-[130px] md:mt-32 sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1025px] xl:max-w-[1280px] 2xl:max-w-[1536px] scale-[101%]"
     /> -->
@@ -7,7 +7,7 @@
     <h1 class="text-5xl font-bold my-10 text-center md:text-left z-50">
       {{ $t('testimonials.title') }}
     </h1>
-    <div class="flex animate-slide whitespace-nowrap gap-[24px] ml-[-350px] mb-5">
+    <div class="flex animate-slide whitespace-nowrap gap-[24px] ml-[-650px] mb-5">
       <!-- ml is set to -350px to prevent blocks from getting squished -->
       <div
         v-for="testimonial in topTestimonials"
@@ -22,7 +22,24 @@
           </h2>
           <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
             <h3>{{ testimonial.name }}</h3>
-            <p>{{ testimonial.position }}{{ testimonial.company }}</p>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
+          </div>
+        </div>
+      </div>
+      <div
+        v-for="testimonial in topTestimonials"
+        :key="testimonial.text"
+        class="w-full inline-block whitespace-normal"
+      >
+        <div
+          class=" flex flex-col w-[350px] h-full p-7 justify-between rounded-2xl bg-gradient-to-t from-transparent backdrop-blur-md backdrop-brightness-105 to-[#393939] mb-6"
+        >
+          <h2 class="text-2xl">
+            {{ testimonial.text }}
+          </h2>
+          <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
+            <h3>{{ testimonial.name }}</h3>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
           </div>
         </div>
       </div>
@@ -39,7 +56,7 @@
           </h2>
           <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
             <h3>{{ testimonial.name }}</h3>
-            <p>{{ testimonial.position }}{{ testimonial.company }}</p>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
           </div>
         </div>
       </div>
@@ -56,12 +73,12 @@
           </h2>
           <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
             <h3>{{ testimonial.name }}</h3>
-            <p>{{ testimonial.position }}{{ testimonial.company }}</p>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="flex animate-slide whitespace-nowrap gap-[24px] ml-[-175px]">
+    <div class="flex animate-slide whitespace-nowrap gap-[24px] ml-[-475px]">
       <div
         v-for="testimonial in bottomTestimonials"
         :key="testimonial.text"
@@ -75,7 +92,7 @@
           </h2>
           <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
             <h3>{{ testimonial.name }}</h3>
-            <p>{{ testimonial.position }}{{ testimonial.company }}</p>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
           </div>
         </div>
       </div>
@@ -92,7 +109,7 @@
           </h2>
           <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
             <h3>{{ testimonial.name }}</h3>
-            <p>{{ testimonial.position }}{{ testimonial.company }}</p>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
           </div>
         </div>
       </div>
@@ -109,7 +126,24 @@
           </h2>
           <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
             <h3>{{ testimonial.name }}</h3>
-            <p>{{ testimonial.position }}{{ testimonial.company }}</p>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
+          </div>
+        </div>
+      </div>
+      <div
+        v-for="testimonial in bottomTestimonials"
+        :key="testimonial.text"
+        class="w-full inline-block whitespace-normal"
+      >
+        <div
+          class="flex flex-col w-[350px] h-full p-7 justify-between rounded-2xl bg-gradient-to-t from-transparent backdrop-blur-md backdrop-brightness-105 to-[#393939]"
+        >
+          <h2 class="text-2xl">
+            {{ testimonial.text }}
+          </h2>
+          <div class="flex flex-col text-[color:var(--tertiary-text-color)]">
+            <h3>{{ testimonial.name }}</h3>
+            <p>{{ testimonial.position }} {{ testimonial.company }}</p>
           </div>
         </div>
       </div>
