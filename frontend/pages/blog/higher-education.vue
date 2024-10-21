0531ca919c0c4e9fdfcff9b928185082c27106ad
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-10-21 12:12:51 +0200

    add header and dates

---
+++ frontend/pages/blog/higher-education.vue
@@ -0,0 +1,25 @@
+<template>
+  <BlogPost
+    title="Dlaczego zacząłem edukację wyższą i dlaczego warto?"
+    dates="2024-10-21"
+  >
+    <template #content />
+  </BlogPost>
+</template>
+
+<script setup lang="ts">
+const { t, locale } = useI18n()
+
+watch(locale, () => {
+    useHead({
+        title: t('blog.higherEducation.meta.title'),
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: t('blog.higherEducation.meta.description'),
+            },
+        ],
+    })
+}, { immediate: true })
+</script>
