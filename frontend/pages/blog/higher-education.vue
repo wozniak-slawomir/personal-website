cc6df4e9b2c4a69fd1c58cc318c163e58b46c526
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-10-21 14:33:27 +0200

    add basic post

--- frontend/pages/blog/higher-education.vue
+++ frontend/pages/blog/higher-education.vue
@@ -1,9 +1,235 @@
 <template>
   <BlogPost
-    title="Dlaczego zacząłem edukację wyższą i dlaczego warto?"
+    title="Co edukacja wyższa robi z człowiekiem?"
     dates="2024-10-21"
   >
-    <template #content />
+    <template #content>
+      <p>Jak sobie radzisz z samodzielną pracą?</p>
+      <p class="mt-2">
+        Jesteś zdolny/a nauczyć się samodzielnie języka? Nowej technologii? Nowego narzędzia?
+      </p>
+      <p>Czy jak kupujesz sobie nowe urządzenie, to z przymusu, czy z ekscytacją poznajesz jego możliwości?</p>
+
+      <p class="mt-4">
+        Może tak jak ja sam wykształciłeś w sobie specjalistyczne umiejętności, imponującą komuś wiedzę lub zdolności, które są warte podziwu?
+      </p>
+
+      <p class="mt-4">
+        Gratuluję. Masz dostęp do złotego narzędzia o które wielu walczy i pragnie.
+      </p>
+
+      <figure>
+        <img
+          src="~assets/blog/higher-education/handstand.jpg"
+          alt="handstand"
+          class="mt-8 w-64 m-auto"
+        >
+        <figcaption class="text-center mt-2">
+          Tak, nauczyłem się tego sam i to dość łatwo.
+        </figcaption>
+      </figure>
+
+      <p class="mt-8">
+        Ale ten post nie jest stricte o mnie. Nie jest on od chwalenia się (choć z pewnością niejeden będzie).
+      </p>
+
+      <p class="mt-4">
+        Przez bardzo długi czas z dumą i zadowoleniem rozwijałem się samodzielnie, korzystając z różnych źródeł.
+        <br>Nie byłem fanem szkoły wyższej. Nie widziałem tam siebie.
+      </p>
+
+      <p class="mt-4">
+        I wiesz co? 
+      </p>
+
+      <p class="mt-4">
+        Teraz jestem. I to jak.
+      </p>
+
+      <p class="mt-4">
+        Gdzieś na początku marca 2024 spontanicznie stwierdziłem że zapiszę się na kierunek Psychologia w Biznesie na uniwersytecie Merito.
+      </p>
+
+      <img
+        src="~assets/blog/higher-education/enrollment-confirmation.png"
+        alt="potwierdzenie zapisu na studia"
+        class="mt-8 w-128 m-auto"
+      >
+
+      <p class="mt-4">
+        Zawsze można się wypisać przecież, nic nie tracę.
+      </p>
+
+      <p class="mt-4">
+        I oh boy, ile zyskałem w ciągu tych kilku miesięcy (jestem dopiero na 2 semestrze)!
+      </p>
+
+      <p class="mt-4">
+        Skupię się tutaj na aspektach mniej oczywistych.
+      </p>
+
+      <h2 class="mt-4">
+        <strong>1. Wartość społeczna</strong>
+      </h2>
+
+      <p class="mt-4">
+        Spodziewałem się że poznam fajne osoby. Ale znacznie przewyższyło to moje oczekiwania.
+      </p>
+
+      <p class="mt-4">
+        Trafiłem na grupę w której niejedna osoba prowadzi własną działalność gospodarczą. Ludzie mają własne kariery, zarządzają zespołami, prowadzą projekty.
+        Tworzy to niesamowite możliwości co do współpracy biznesowej.
+      </p>
+
+      <img
+        src="~assets/blog/higher-education/friendship.jpg"
+        alt="me"
+        class="mt-8 w-64 m-auto"
+      >
+
+      <p class="mt-4">
+        Z dwójką z nich już zbudowałem w przeciągu 7 miesięcy bliską relację w której wspieramy się personalnie i biznesowo. Pozdrawiam Klaudia i Piotrek :)
+        Jestem pewny że to dopiero początek.
+      </p>
+
+      <p class="mt-4">
+        Wniosek:
+        <strong class="underline">poznasz osoby które zmienią twoje życie.</strong>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>2. Pomysły, idee</strong>
+      </h2>
+
+      <p class="mt-4">
+        O wiedzy i tym co zyskasz porozmawiamy później. Teraz chciałbym zwrócić uwagę na coś innego.
+      </p>
+
+      <p class="mt-4">
+        W trakcie zajęć i poza nimi, spotkasz się z pracami, projektami i ludźmi którzy są na najwyższej półce intelektualnej.<br>
+        Nie zliczę ile pomysłów i inspiracji zebrałem w ciągu tych kilku miesięcy.
+      </p>
+
+      <p class="mt-4">
+        Dla przykładu: spontanicznie zobaczyłem plakat odnośnie konkursu na projekt IT związany z AI.
+        Poświęciłem minimalną ilość czasu na zbudowanie konceptu bo zajęło mi to koło godziny.
+      </p>
+
+      <img
+        src="~assets/blog/higher-education/contest.png"
+        alt="me"
+        class="mt-8 w-128 m-auto"
+      >
+
+      <p class="mt-4">
+        Czy wygrałem? Nie.
+      </p>
+
+      <p class="mt-4">
+        Ale projekt zacząłem i tak, bo widzę w nim potencjał. Jestem w trakcie implementacji, uczę się zarządzania projektem i zespołem.
+        Planuję go wdrożyć w życie, wypuścić w świat i może nawet kiedyś zmonetyzować. Więcej informacji o tym za jakiś czas :)
+      </p>
+
+      <p class="mt-4">
+        Wniosek:
+        <strong class="underline">zainspirujesz się i będziesz mieć więcej perspektyw do działania.</strong>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>3. Sposób zdobywania wiedzy</strong>
+      </h2>
+
+      <p class="mt-4">
+        Jeżeli uczysz się samodzielnie, to wiesz z pewnością że jest to proces wymagający.
+        Musisz nie tylko przyswoić wiedzę, ale mieć też plan i kontrolować cały proces.
+      </p>
+
+      <p class="mt-4">
+        W szkole wyższej masz to wszystko ułatwione. Masz plan przygotowany przez specjalistów, masz podręczniki które ktoś pisał miesiącami, masz wsparcie ludzi mądrzejszych od ciebie.
+      </p>
+
+      <p class="mt-4">
+        Delegując proces planowania i kontrolowania, możesz skupić się na innych czynnościach. Na przykład przyswajaniu wiedzy, ale też na prywatnych aspektach życia.
+      </p>
+
+      <p class="mt-4">
+        Wniosek:
+        <strong class="underline">będziesz uczyć się efektywniej.</strong>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>4. Twój zasób słownictwa</strong>
+      </h2>
+
+      <p class="mt-4">
+        W trakcie zajęć poznasz nowe słowa, nowe pojęcia, nowe definicje.
+        Na jednych zajęciach trzeba było dokonać przemowę. Przełamać lęk w sytuacji która wielu paraliżuje.
+      </p>
+
+      <p class="mt-4">
+        Ale mnie uderzyło coś innego.
+      </p>
+
+      <p class="mt-4">
+        Po miesiącach nauki, zauważyłem drastyczną zmianę mojej mowy.<br>
+        Gdy miałem rozmowy o pracę, czułem że mówię z przekonaniem, z wiedzą, z pewnością siebie.<br>
+        Gdy rozmawiałem z przyjaciółmi, byłem spokojniejszy, bardziej elokwentny, okazywałem więcej miłości.<br>
+      </p>
+
+      <figure>
+        <img
+          src="~assets/blog/higher-education/interview.png"
+          alt="interview"
+          class="mt-8 w-64 m-auto"
+        >
+        <figcaption class="text-center mt-2">
+          Poczujesz się pewniej.
+        </figcaption>
+      </figure>
+
+      <p class="mt-4">
+        Wiedziałem czego chcę i jak to uzasadnić. Potrafiłem sprzedać siebie i swoje wartości.
+      </p>
+
+      <p class="mt-4">
+        Wniosek:
+        <strong class="underline">twoja charyzma i umiejętność przekazu wzrośnie.</strong>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>5. Wartość wiedzy</strong>
+      </h2>
+
+      <p class="mt-4">
+        Wiesz doskonale o co chodzi. Mieliła ci to babcia, mama, ojciec i nauczyciele w szkole. Nie jest moim celem to powtarzać.
+      </p>
+
+      <p class="mt-4">
+        Jedynie dodam że jesteśmy zbiorem naczyń połączonych. Wiedza którą posiądziesz z pewnością wpłynie na Twoje życie.
+      </p>
+
+      <p class="mt-4">
+        Może zbudujesz tożsamość osoby inteligentnej, która ma siłę i jest zdolna do wielkich rzeczy?<br>
+        Może będziesz wiedziała więcej o zachowaniu ludzi i nie będziesz stosować kar i przemocy wobec swoich dzieci, bo wiesz jak szkodliwe to jest?<br>
+        A może dowiesz się że nie zawsze warto brać leki przeciwzapalne gdy masz gorączkę?
+      </p>
+
+      <p class="mt-4">
+        Wniosek:
+        <strong class="underline">będziesz mądrzejszy/a.</strong>
+      </p>
+
+      <hr class="mt-8">
+
+      <p class="mt-4">
+        Moim celem było przybliżyć Tobie kilka nieoczywistych aspektów studiów, jak i podzielić się moim doświadczeniem.
+        Sporo się zmieniło, i myślę że warto dzielić się swoimi przemyśleniami.
+      </p>
+
+      <p class="mt-4">
+        Może jednak babcia miała rację.
+      </p>
+    </template>
   </BlogPost>
 </template>
 
