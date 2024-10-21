5b46c27772b4176b665c652d8ec3d5524f99521d
Slawomir Wozniak <wozniak.slawomir.contact@gmail.com>
2024-10-21 17:57:43 +0200

    University post (#57)

--- frontend/components/RealizedProjects.vue
+++ frontend/components/RealizedProjects.vue
@@ -5,38 +5,59 @@
         {{ $t('projects.title') }}
       </h1>
       <div class="mb-5 mt-10 flex flex-wrap lg:my-10 gap-5">
-        <button :class="{ 'bg-[#474b59]': activeFilter === 'business' }"
+        <button
+          :class="{ 'bg-[#474b59]': activeFilter === 'business' }"
           class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow uppercase lg:max-w-fit lg:px-20 lg:mb-0"
-          @click="activeFilter = activeFilter === 'business' ? 'all' : 'business'">
+          @click="activeFilter = activeFilter === 'business' ? 'all' : 'business'"
+        >
           {{ $t('projects.filter.business') }}
         </button>
-        <button :class="{ 'bg-[#474b59]': activeFilter === 'portfolio' }"
+        <button
+          :class="{ 'bg-[#474b59]': activeFilter === 'portfolio' }"
           class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow uppercase lg:max-w-fit lg:px-20 lg:mb-0"
-          @click="activeFilter = activeFilter === 'portfolio' ? 'all' : 'portfolio'">
+          @click="activeFilter = activeFilter === 'portfolio' ? 'all' : 'portfolio'"
+        >
           {{ $t('projects.filter.portfolio') }}
         </button>
-        <button :class="{ 'bg-[#474b59]': activeFilter === 'blog' }"
+        <button
+          :class="{ 'bg-[#474b59]': activeFilter === 'blog' }"
           class="text-l p-3 flex-1 rounded-3xl bg-[#393939] hover:bg-[#464646] h-15 w-full duration-300 shadow uppercase lg:max-w-fit lg:px-20 lg:mb-0"
-          @click="activeFilter = activeFilter === 'blog' ? 'all' : 'blog'">
+          @click="activeFilter = activeFilter === 'blog' ? 'all' : 'blog'"
+        >
           {{ $t('projects.filter.blog') }}
         </button>
       </div>
-      <masonry-wall :items="filteredItems" :gap="24" :min-columns="columnsNum" :rtl="rtl">
+      <masonry-wall
+        :items="filteredItems"
+        :gap="24"
+        :min-columns="columnsNum"
+        :rtl="rtl"
+      >
         <template #default="{ item }">
           <div class="relative rounded-2xl overflow-hidden group min-h-fit">
-            <a :href="item.link" target="_blank">
-              <img :src="item.image" :alt="item.name" class="w-full h-full object-cover max-h-[800px]"
-                @load="fixLibraryBug">
+            <NuxtLink
+              :to="item.link"
+            >
+              <img
+                :src="item.image"
+                :alt="item.name"
+                class="w-full h-full object-cover max-h-[800px]"
+                @load="fixLibraryBug"
+              >
               <div
-                class="bottom-0 left-0 right-0 backdrop-blur-md bg-[#393939] p-5 opacity-100 group-hover:opacity-100 transition-opacity duration-300 xl:backdrop-brightness-50 xl:opacity-0 xl:absolute xl:bg-transparent">
+                class="bottom-0 left-0 right-0 backdrop-blur-md bg-[#393939] p-5 opacity-100 group-hover:opacity-100 transition-opacity duration-300 xl:backdrop-brightness-50 xl:opacity-0 xl:absolute xl:bg-transparent"
+              >
                 <h1 class="text-2xl font-bold text-white uppercase">
                   {{ item.name }}
                 </h1>
-                <p class="text-white my-2">
+                <p
+                  v-if="item.description"
+                  class="text-white my-2"
+                >
                   {{ item.description }}
                 </p>
               </div>
-            </a>
+            </NuxtLink>
           </div>
         </template>
       </masonry-wall>
@@ -146,6 +167,12 @@ const items = computed(() => [
     description: t('projects.piotrchojankowski'),
     tags: ['business', 'portfolio'],
     link: 'https://www.piotrchojankowski.pl',
+  },
+  {
+    name: t('projects.blog.higherEducation'),
+    image: new URL('../assets/projects/blog/higher-education.jpg', import.meta.url).href,
+    tags: ['blog'],
+    link: '/blog/higher-education',
   },
 ])
 
---
+++ frontend/pages/blog/higher-education.vue
@@ -0,0 +1,238 @@
+<template>
+  <BlogPost
+    :title="t('blog.higherEducation.title')"
+    :dates="t('blog.higherEducation.dates')"
+  >
+    <template #content>
+      <p>{{ t('blog.higherEducation.content1') }}</p>
+      <p class="mt-2">
+        {{ t('blog.higherEducation.content2') }}
+      </p>
+      <p>{{ t('blog.higherEducation.content3') }}</p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content4') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content5') }}
+      </p>
+
+      <figure>
+        <img
+          src="~assets/blog/higher-education/handstand.jpg"
+          alt="handstand"
+          class="mt-8 w-64 m-auto"
+        >
+        <figcaption class="text-center mt-2">
+          {{ t('blog.higherEducation.content6') }}
+        </figcaption>
+      </figure>
+
+      <p class="mt-8">
+        {{ t('blog.higherEducation.content7') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content8') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content9') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content10') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content11') }}
+      </p>
+
+      <img
+        src="~assets/blog/higher-education/enrollment-confirmation.png"
+        alt="potwierdzenie zapisu na studia"
+        class="mt-8 w-128 m-auto"
+      >
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content12') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content13') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content14') }}
+      </p>
+
+      <h2 class="mt-4">
+        <strong>{{ t('blog.higherEducation.content15') }}</strong>
+      </h2>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content16') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content17') }}
+      </p>
+
+      <img
+        src="~assets/blog/higher-education/friendship.jpg"
+        alt="me"
+        class="mt-8 w-64 m-auto"
+      >
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content18') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.conclusion') }}
+        <span class="font-semibold underline">{{ t('blog.higherEducation.content19') }}</span>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>{{ t('blog.higherEducation.content20') }}</strong>
+      </h2>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content21') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content22') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content23') }}
+      </p>
+
+      <img
+        src="~assets/blog/higher-education/contest.png"
+        alt="me"
+        class="mt-8 w-128 m-auto"
+      >
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content24') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content25') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.conclusion') }}
+        <span class="font-semibold underline">{{ t('blog.higherEducation.content26') }}</span>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>{{ t('blog.higherEducation.content27') }}</strong>
+      </h2>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content28') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content29') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content30') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.conclusion') }}
+        <span class="font-semibold underline">{{ t('blog.higherEducation.content31') }}</span>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>{{ t('blog.higherEducation.content32') }}</strong>
+      </h2>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content33') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content34') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content35') }}
+      </p>
+
+      <figure>
+        <img
+          src="~assets/blog/higher-education/interview.png"
+          alt="interview"
+          class="mt-8 w-64 m-auto"
+        >
+        <figcaption class="text-center mt-2">
+          {{ t('blog.higherEducation.content36') }}
+        </figcaption>
+      </figure>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content37') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.conclusion') }}
+        <span class="font-semibold underline">{{ t('blog.higherEducation.content38') }}</span>
+      </p>
+
+      <h2 class="mt-4">
+        <strong>{{ t('blog.higherEducation.content39') }}</strong>
+      </h2>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content40') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content41') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content42') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.conclusion') }}
+        <span class="font-semibold underline">{{ t('blog.higherEducation.content43') }}</span>
+      </p>
+
+      <hr class="mt-8">
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content44') }}
+      </p>
+
+      <p class="mt-4">
+        {{ t('blog.higherEducation.content45') }}
+      </p>
+    </template>
+  </BlogPost>
+</template>
+
+<script setup lang="ts">
+const { t, locale } = useI18n()
+
+watch(locale, () => {
+    useHead({
+        title: t('blog.higherEducation.meta.title'),
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: t('blog.higherEducation.meta.description'),
+            },
+        ],
+    })
+}, { immediate: true })
+</script>
