7a91159d99f39e9123d1a64a9fb6b37467442897
Mikołaj Kondela <mkondela.formal@gmail.com>
2025-01-05 22:16:58 +0100

    Fix navbar content width

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,47 +1,49 @@
 <template>
   <nav
-    class="w-full h-[var(--navbar-height)] bg-[color:var(--secondary-color)] py-5 px-16 flex justify-between items-center fixed top-0 left-0 z-20"
+    class="w-full h-[var(--navbar-height)] bg-[color:var(--secondary-color)] py-5 flex fixed top-0 z-20"
   >
-    <NuxtImg
-      src="/logo.png"
-      class="max-w-[400px] min-w-[100px] mr-3 md:mr-0"
-      :alt="$t('alt.slawomir')"
-      width="400"
-      height="30"
-    />
-    <div class="flex">
-      <div
-        class="p-3 bg-[#393939] cursor-pointer hover:bg-[#474b59] w-14 text-center sm:w-24 h-min z-20"
-        :class="isMenuOpen ? 'rounded-t-[25px]' : 'rounded-[25px]'"
-        @click=" isMenuOpen = !isMenuOpen"
-      >
-        <span
-          class="fi fis rounded-full w-6 h-6 align-middle mx-1"
-          :class="locale === 'en' ? 'fi-gb' : 'fi-' + locale"
-        />
-        <p class="align-middle mx-1 hidden sm:inline uppercase">
-          {{ locale }}
-        </p>
-      </div>
-      <div
-        v-if="isMenuOpen"
-        v-on-click-outside="() => isMenuOpen = false"
-        class="absolute rounded-[25px] top-8 pt-10 pb-2 bg-[#393939] z-10 sm:w-24"
-      >
-        <div class="flex flex-col">
-          <!-- weird bug in library -->
-          <button
-            v-for="item in (locales as unknown as string[])"
-            :key="item"
-            class="p-2 rounded-md w-14 text-center sm:text-left sm:pl-[18px] hover:font-bold sm:w-full hover:scale-110 duration-150"
-            @click="changeLanguage(item)"
-          >
-            <span
-              class="fi fis rounded-full w-6 h-6 align-middle mx-1"
-              :class="item === 'en' ? 'fi-gb' : 'fi-' + item"
-            />
-            <span class="align-middle mx-1 hidden sm:inline uppercase">{{ item }}</span>
-          </button>
+    <div class="container justify-between items-center flex">
+      <NuxtImg
+        src="/logo.png"
+        class="max-w-[400px] min-w-[100px] mr-3 md:mr-0"
+        :alt="$t('alt.slawomir')"
+        width="400"
+        height="30"
+      />
+      <div class="flex">
+        <div
+          class="p-3 bg-[#393939] cursor-pointer hover:bg-[#474b59] w-14 text-center sm:w-24 h-min z-20"
+          :class="isMenuOpen ? 'rounded-t-[25px]' : 'rounded-[25px]'"
+          @click=" isMenuOpen = !isMenuOpen"
+        >
+          <span
+            class="fi fis rounded-full w-6 h-6 align-middle mx-1"
+            :class="locale === 'en' ? 'fi-gb' : 'fi-' + locale"
+          />
+          <p class="align-middle mx-1 hidden sm:inline uppercase">
+            {{ locale }}
+          </p>
+        </div>
+        <div
+          v-if="isMenuOpen"
+          v-on-click-outside="() => isMenuOpen = false"
+          class="absolute rounded-[25px] top-8 pt-10 pb-2 bg-[#393939] z-10 sm:w-24"
+        >
+          <div class="flex flex-col">
+            <!-- weird bug in library -->
+            <button
+              v-for="item in (locales as unknown as string[])"
+              :key="item"
+              class="p-2 rounded-md w-14 text-center sm:text-left sm:pl-[18px] hover:font-bold sm:w-full hover:scale-110 duration-150"
+              @click="changeLanguage(item)"
+            >
+              <span
+                class="fi fis rounded-full w-6 h-6 align-middle mx-1"
+                :class="item === 'en' ? 'fi-gb' : 'fi-' + item"
+              />
+              <span class="align-middle mx-1 hidden sm:inline uppercase">{{ item }}</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
