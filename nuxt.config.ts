cbb8ec6cc114ae66569ef43ead1aa2eec218215d
Sławomir <contact@slawomir-wozniak.pl>
2025-12-20 16:02:56 +0100

    add meta pixel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -125,6 +125,11 @@ export default defineNuxtConfig({
         },
         {
           innerHTML: `
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '686040061255221'); fbq('track', 'PageView');
+          `,
+        },
+        {
+          innerHTML: `
             // Fallback font loading for critical FCP
             if (!document.fonts || !document.fonts.check('1em Outfit')) {
               var link = document.createElement('link');
