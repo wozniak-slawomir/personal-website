3830e95b0efe9d07d0fe570381dbc13ff97b7dad
Sławomir <contact@slawomir-wozniak.pl>
2025-12-24 23:55:33 +0100

    add page transition

--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="container">
-    <BackButton class="mt-36" />
-  </div>
-  <div class="max-w-2xl container mt-16 text-justify">
-    <h1 class="text-4xl font-bold text-center">
-      {{ props.title }}
-    </h1>
-
-    <div class="text-center mt-2">
-      {{ props.dates }}
+  <div>
+    <div class="container">
+      <BackButton class="mt-36" />
     </div>
+    <div class="max-w-2xl container mt-16 text-justify">
+      <h1 class="text-4xl font-bold text-center">
+        {{ props.title }}
+      </h1>
 
-    <div class="text-center mt-2">
-      {{ props.tags.map(el => `#${el}`).join(' ') }}
-    </div>
+      <div class="text-center mt-2">
+        {{ props.dates }}
+      </div>
 
-    <div class="glassmorphism mt-16 py-4 rounded-xl px-8">
-      <slot name="content" />
+      <div class="text-center mt-2">
+        {{ props.tags.map(el => `#${el}`).join(' ') }}
+      </div>
+
+      <div class="glassmorphism mt-16 py-4 rounded-xl px-8">
+        <slot name="content" />
+      </div>
+      <CTASection />
     </div>
-    <CTASection />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { defineProps } from 'vue'
-
 const props = defineProps<{
     title: string
     dates: string
